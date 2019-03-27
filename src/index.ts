bec698143e30769d9c9fcbfcc82902f563da78d2
David LJ <mail@davidlj95.com>
2019-03-27 10:26:51 +0100

    Test nested types detection

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import "reflect-metadata";
 import "es6-shim";
-import {plainToClass} from "class-transformer";
+import {plainToClass, Type} from "class-transformer";
 
-class User {
-    id: number;
-    firstName: string;
-    lastName: string;
-    age: number;
-
-    constructor(id: number, firstName: string, lastName: string, age: number) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
+class Address {
+    constructor(public street: string, public city: string) {
     }
 
+    isNear() {
+        return this.city === "Andorra"
+    }
+}
+
+class User {
+    @Type(() => Address) public address: Address;
+
+    constructor(
+        public id: number,
+        public firstName: string,
+        public lastName: string,
+        public age: number,
+        address: Address) {
+        this.address = address;
+    }
 
     getName() {
         return this.firstName + " " + this.lastName;
@@ -30,21 +37,36 @@ let usersData = [
         "id": 1,
         "firstName": "Johny",
         "lastName": "Cage",
-        "age": 27
+        "age": 27,
+        "address": {
+            "street": "Elm",
+            "city": "London"
+        }
     },
     {
         "id": 2,
         "firstName": "Ismoil",
         "lastName": "Somoni",
-        "age": 50
+        "age": 50,
+        "address": {
+            "street": "Girona",
+            "city": "Barcelona"
+        }
     },
     {
         "id": 3,
         "firstName": "Luke",
         "lastName": "Dacascos",
-        "age": 12
+        "age": 12,
+        "address": {
+            "street": "Unió",
+            "city": "Andorra"
+        }
     }];
 
 const users = plainToClass(User, usersData);
 
-users.forEach((user) => { console.log(user.isAdult())});
+users.forEach((user) => {
+    console.log(`${user.firstName} is ${user.isAdult() ? "adult" : "young or elder"}`);
+    console.log(`   and lives in ${user.address.city} (near: ${user.address.isNear()})`)
+});
